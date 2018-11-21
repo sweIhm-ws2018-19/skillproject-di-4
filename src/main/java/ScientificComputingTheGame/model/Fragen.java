8045d104e13a10e24d5ca2bc4025553d4584a3c1
FlorianLandmann <43856261+FlorianLandmann@users.noreply.github.com>
2018-11-21 12:40:29 +0100

    Fragen und Kurse hinzugefuegt

---
+++ src/main/java/ScientificComputingTheGame/model/Fragen.java
@@ -0,0 +1,17 @@
+package main.java.ScientificComputingTheGame.model;
+
+public abstract class Fragen {
+	public abstract void frageStellen();
+	public abstract void frageWiederholen();
+	public abstract boolean antwortPruefen(String userSolution);
+	
+	public Fragen() {
+		//TODO 
+		/*
+		 * select random question
+		 * frageStellen()
+		 * frageWiederholen() or antwortPruefen()
+		 * ...
+		 */
+	}
+}
