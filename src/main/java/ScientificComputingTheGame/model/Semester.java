6f6c5bbe67f84f1347095f2fe9d3391f8f638bae
jonasch98 <jonasch@hm.edu>
2018-11-21 12:22:15 +0100

    Semester nochmals hinzugefügt

---
+++ src/main/java/ScientificComputingTheGame/model/Semester.java
@@ -0,0 +1,14 @@
+package main.java.ScientificComputingTheGame.model;
+
+import java.util.ArrayList;
+
+public class Semester {
+	public ArrayList<Kurs> kurse = new ArrayList<Kurs>();
+	public int id;
+	
+	public Semester(int id) {
+		this.id = id;
+	}
+	
+	
+}
