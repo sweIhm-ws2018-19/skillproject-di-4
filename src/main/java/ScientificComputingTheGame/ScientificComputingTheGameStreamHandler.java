7e888680f8057b8695dbc9727b38f1315ad2cf10
christovogt <c.vogt0@hm.edu>
2018-11-26 17:01:11 +0100

    Inklusive: keine Fehler & Alexa greift auf Model-Klassen zu

--- src/main/java/ScientificComputingTheGame/ScientificComputingTheGameStreamHandler.java
+++ src/main/java/ScientificComputingTheGame/ScientificComputingTheGameStreamHandler.java
@@ -25,13 +25,14 @@ import main.java.ScientificComputingTheGame.handlers.HelpIntentHandler;
 import main.java.ScientificComputingTheGame.handlers.LaunchRequestHandler;
 import main.java.ScientificComputingTheGame.handlers.SessionEndedRequestHandler;
 import main.java.ScientificComputingTheGame.handlers.WelcomeIntentHandler;
+import main.java.ScientificComputingTheGame.model.ScientificComputingTheGame;
 import main.java.ScientificComputingTheGame.handlers.CancelandStopIntentHandler;
 
 public class ScientificComputingTheGameStreamHandler extends SkillStreamHandler {
 
     private static Skill getSkill() {
     	int Harry = 0;
-
+    	ScientificComputingTheGame s = new ScientificComputingTheGame();
         return Skills.standard()
                 .addRequestHandlers(
                         new LaunchRequestHandler(),
@@ -39,7 +40,7 @@ public class ScientificComputingTheGameStreamHandler extends SkillStreamHandler 
                         new SessionEndedRequestHandler(),
                         new HelpIntentHandler(),
                         new FallbackIntentHandler(),
-                        new WelcomeIntentHandler())
+                        new WelcomeIntentHandler(s))
                 // Add your skill id below
                 .withSkillId("amzn1.ask.skill.6c52904c-e625-439f-bde3-4322e243883e")
                 .build();
