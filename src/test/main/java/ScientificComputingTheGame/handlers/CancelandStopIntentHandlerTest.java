a05d72afef6c0c5c9b3e5826ea7fee94633e1d85
jonasch98 <jonasch@hm.edu>
2018-12-04 11:40:27 +0100

    updating

---
+++ src/test/main/java/ScientificComputingTheGame/handlers/CancelandStopIntentHandlerTest.java
@@ -0,0 +1,19 @@
+package test.main.java.ScientificComputingTheGame.handlers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CancelandStopIntentHandlerTest {
+
+	@Test
+	void testCanHandle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHandle() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/main/java/ScientificComputingTheGame/model/KursTest.java
@@ -0,0 +1,59 @@
+package test.main.java.ScientificComputingTheGame.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KursTest {
+
+	@Test
+	void testGetAnzahlZuBeantwortenderFragen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFragen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetECTS() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testKurs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPruefungBeginnen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPruefungBewerten() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testKursBeenden() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testKursWiederholen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBestanden() {
+		fail("Not yet implemented");
+	}
+
+}
