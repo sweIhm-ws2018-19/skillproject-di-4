bbc7a6919a808107fd84918aabe2f37e8524f59e
jonasch98 <jonasch@hm.edu>
2019-01-03 17:29:05 +0100

    changes in AnalysisTest

--- src/test/java/ScientificComputingTheGame/model/AlleKurse/AnalysisTest.java
+++ src/test/java/ScientificComputingTheGame/model/AlleKurse/AnalysisTest.java
@@ -25,11 +25,11 @@ public class AnalysisTest {
 		ArrayList<Frage> fragen = analysisTest.getFragen();
 		for(int i=0; i < Analysis.alleFragen.length;i++) {
 			for(int j=0; j < fragen.size();j++)
-			if(Analysis.alleFragen[i][0].equals(fragen.get(j).getFrage()) && Analysis.alleFragen[i][2].equals(fragen.get(j).getAntwort())) {
+			if((Analysis.alleFragen[i][0]+Analysis.alleFragen[i][1]).equals(fragen.get(j).getFrage()) && Analysis.alleFragen[i][2].equals(fragen.get(j).getAntwort())) {
 				equals++;
 			}
 		}
-		if(equals == fragen.size()-1) {
+		if(equals == fragen.size()) {
 			return true;
 		}else {
 			return false;
