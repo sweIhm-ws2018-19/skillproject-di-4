f2f486bb86e92ba8683507de53125c96340e1c13
jonasch98 <jonasch@hm.edu>
2019-01-04 17:38:02 +0100

    added LineareAlgebraTest

--- src/main/java/ScientificComputingTheGame/model/AlleKurse/LineareAlgebra.java
+++ src/main/java/ScientificComputingTheGame/model/AlleKurse/LineareAlgebra.java
@@ -4,8 +4,22 @@ import main.java.ScientificComputingTheGame.model.Frage;
 import main.java.ScientificComputingTheGame.model.Kurs;
 
 public class LineareAlgebra extends Kurs {
-	private String name = "LineareAlgebra";
+	public static final String name = "LineareAlgebra";
 	private final int anzahlZuBeantwortenderFragen;
+	public static final String[][] alleFragen = {
+			{"Was erhaelt man wenn man einen Zeilenvektor transponiert?", "einen Zeilenvektor, einen Spaltenvektor, eine Matrix, die Einheitsmatrix", "Spaltenvektor"}, 
+			{"Was erhaelt man wenn man einen Zeilenvektor mit einem Spaltenvektor multipliziert?", "einen Zeilenvektor, einen Spaltenvektor, eine Matrix, die Einheitsmatrix", "Matrix"},
+			{"Wofuer stehen L und U bei der L U zerlegung?", "A: Ludwig Unger Methode, B: Lower und Upper Matrix, C: Loewe und Uhu Matrix, D: Lettland und Ungarn", "B"},
+			{"Was ist der Rang einer Matrix?", "A: Die Anzahl der Zeilen, B: Die Anzahl der Spalten, C: Die Anzahl der Zeilen, D: Die Anzahl der liear unabhaengigen Zeilen und Spalten", "D"},
+			{"Was ist die Determinante der 4 Kreuz 4 Einheitsmatix?", "4, 1, 16, 0", "Eins"},
+			{"Was ist das quadrat der komplexen Zahl i?", "A: minus 1, B: 1, C: 0, D: nicht definiert", "A"},
+			{"Gegeben ist das Gleichungssystem 3 mal x plus 2 y ist gleich 10 und 6 mal x plus 4 mal y ist gleich z. Fuer welchen Wert von z hat das System keine Loesung?", "", "zehn"},
+			{"Gegeben ist das Gleichungssystem 3 mal x plus 2 y ist gleich 10 und 6 mal x plus 4 mal y ist gleich z. Fuer welchen Wert von z hat das System unendlich viele Loesungen?", "", "zehn"}};
+	public static final String beschreibung = "Im Kurs Lineare Algebra lernen die Studierenden wichtige "
+			+ "grundlegende Begriffe und Methoden die sie unter anderem in der "
+			+ "Numerik benoetigen. Behandelt werden unter anderem Vektorraeume, "
+			+ "lineare Gleichungssysteme, Skalarprodukt, Eigenwerte und L U "
+			+ "Zerlegung.";
 
 	public LineareAlgebra() {
 		super();
@@ -14,15 +28,6 @@ public class LineareAlgebra extends Kurs {
 	
 	@Override
 	public ArrayList<Frage> getFragen() {
-		String[][] alleFragen = {
-				{"Was erhaelt man wenn man einen Zeilenvektor transponiert?", "einen Zeilenvektor, einen Spaltenvektor, eine Matrix, die Einheitsmatrix", "Spaltenvektor"}, 
-				{"Was erhaelt man wenn man einen Zeilenvektor mit einem Spaltenvektor multipliziert?", "einen Zeilenvektor, einen Spaltenvektor, eine Matrix, die Einheitsmatrix", "Matrix"},
-				{"Wofuer stehen L und U bei der L U zerlegung?", "A: Ludwig Unger Methode, B: Lower und Upper Matrix, C: Loewe und Uhu Matrix, D: Lettland und Ungarn", "B"},
-				{"Was ist der Rang einer Matrix?", "A: Die Anzahl der Zeilen, B: Die Anzahl der Spalten, C: Die Anzahl der Zeilen, D: Die Anzahl der liear unabhaengigen Zeilen und Spalten", "D"},
-				{"Was ist die Determinante der 4 Kreuz 4 Einheitsmatix?", "4, 1, 16, 0", "Eins"},
-				{"Was ist das quadrat der komplexen Zahl i?", "A: minus 1, B: 1, C: 0, D: nicht definiert", "A"},
-				{"Gegeben ist das Gleichungssystem 3 mal x plus 2 y ist gleich 10 und 6 mal x plus 4 mal y ist gleich z. Fuer welchen Wert von z hat das System keine Loesung?", "", "zehn"},
-				{"Gegeben ist das Gleichungssystem 3 mal x plus 2 y ist gleich 10 und 6 mal x plus 4 mal y ist gleich z. Fuer welchen Wert von z hat das System unendlich viele Loesungen?", "", "zehn"}};
 		fragen = new ArrayList<Frage>();
 		ArrayList<Integer> zufaelligeFragen = new ArrayList<Integer>();
 		int zufaelligeFrageID;
@@ -83,10 +88,6 @@ public class LineareAlgebra extends Kurs {
 	}
 	
 	public String getBeschreibung() {
-		return "Im Kurs Lineare Algebra lernen die Studierenden wichtige "
-				+ "grundlegende Begriffe und Methoden die sie unter anderem in der "
-				+ "Numerik benoetigen. Behandelt werden unter anderem Vektorraeume, "
-				+ "lineare Gleichungssysteme, Skalarprodukt, Eigenwerte und L U "
-				+ "Zerlegung.";
+		return beschreibung;
 	}
 }
---
+++ src/test/java/ScientificComputingTheGame/model/AlleKurse/LineareAlgebraTest.java
@@ -0,0 +1,50 @@
+package test.java.ScientificComputingTheGame.model.AlleKurse;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import main.java.ScientificComputingTheGame.model.Frage;
+import main.java.ScientificComputingTheGame.model.AlleKurse.LineareAlgebra;
+
+public class LineareAlgebraTest {
+
+	LineareAlgebra linAlgTest = new LineareAlgebra();
+
+	@Test
+	public void testAllMethods() {
+		linAlgTest.setAnzahlGestellterFragen(2);
+		linAlgTest.setAnzahlRichtigBeantworteterFragen(2);
+		linAlgTest.setIstPruefungsFrage(true);
+		Assert.assertTrue(testGetFragen());
+		Assert.assertSame(2, linAlgTest.getAnzahlGestellterFragen());
+		Assert.assertSame(2, linAlgTest.getAnzahlRichtigBeantworteterFragen());
+		Assert.assertTrue(linAlgTest.getIstPruefungsFrage());
+		Assert.assertSame(LineareAlgebra.name, linAlgTest.getName());
+		Assert.assertSame(2, linAlgTest.getAnzahlZuBeantwortenderFragen());
+		Assert.assertSame(5, linAlgTest.getECTS());
+		Assert.assertSame(LineareAlgebra.beschreibung, linAlgTest.getBeschreibung());
+	}
+
+	public boolean testGetFragen() {
+		int equals = 0;
+		ArrayList<Frage> fragen = linAlgTest.getFragen();
+		for (int i = 0; i < LineareAlgebra.alleFragen.length; i++) {
+			for (int j = 0; j < fragen.size(); j++)
+				if ((LineareAlgebra.alleFragen[i][0] + " Die Antwortmoeglichkeiten sind: " + LineareAlgebra.alleFragen[i][1])
+						.equals(fragen.get(j).getFrage())
+						&& LineareAlgebra.alleFragen[i][2].equals(fragen.get(j).getAntwort())) {
+					equals++;
+				}
+		}
+		if (equals == fragen.size()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
