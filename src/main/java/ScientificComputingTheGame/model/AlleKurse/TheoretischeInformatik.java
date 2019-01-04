4c7c8aae87e4b034dd2f17418c5ddbd7bdb29511
jonasch98 <jonasch@hm.edu>
2019-01-04 18:02:29 +0100

    added TheoretischeInformatikTest

--- src/main/java/ScientificComputingTheGame/model/AlleKurse/TheoretischeInformatik.java
+++ src/main/java/ScientificComputingTheGame/model/AlleKurse/TheoretischeInformatik.java
@@ -1,40 +1,43 @@
 package main.java.ScientificComputingTheGame.model.AlleKurse;
+
 import java.util.ArrayList;
 import main.java.ScientificComputingTheGame.model.Frage;
 import main.java.ScientificComputingTheGame.model.Kurs;
 
 public class TheoretischeInformatik extends Kurs {
-	private String name = "Theoretische Informatik";
+	public static final String name = "Theoretische Informatik";
 	private final int anzahlZuBeantwortenderFragen;
+	public static final String[][] alleFragen = { { "Frage 1 T I", "Ja, Nein", "Ja" },
+			{ "Frage 2 T I", "Ja, Nein", "Ja" } };
+	public static final String beschreibung = "Das Ziel des Faches Theoretische Informatik ist es den Studierenden "
+			+ "eine Einfuehrung in Automatentheorie, formale Spracehn und " + "Berechenbarkeitstheorie zu geben.";
 
 	public TheoretischeInformatik() {
 		super();
 		anzahlZuBeantwortenderFragen = 2;
 	}
-	
+
 	@Override
 	public ArrayList<Frage> getFragen() {
-		String[][] alleFragen = {
-				{"Frage 1 T I", "Ja, Nein", "Ja"}, 
-				{"Frage 2 T I", "Ja, Nein", "Ja"}};
 		fragen = new ArrayList<Frage>();
 		ArrayList<Integer> zufaelligeFragen = new ArrayList<Integer>();
 		int zufaelligeFrageID;
 		Frage neueFrage;
-		while(zufaelligeFragen.size() < anzahlZuBeantwortenderFragen) {
+		while (zufaelligeFragen.size() < anzahlZuBeantwortenderFragen) {
 			zufaelligeFrageID = (int) (Math.random() * (alleFragen.length));
-			if(!zufaelligeFragen.contains(zufaelligeFrageID)) {
+			if (!zufaelligeFragen.contains(zufaelligeFrageID)) {
 				neueFrage = new Frage(alleFragen[zufaelligeFrageID]);
 				fragen.add(neueFrage);
-				}
 			}
+		}
 		return fragen;
 	}
+
 	@Override
 	public int getAnzahlGestellterFragen() {
 		return anzahlGestellterFragen;
 	}
-	
+
 	@Override
 	public int getAnzahlRichtigBeantworteterFragen() {
 		return anzahlRichtigBeantworteterFragen;
@@ -47,7 +50,7 @@ public class TheoretischeInformatik extends Kurs {
 
 	@Override
 	public void setAnzahlGestellterFragen(int value) {
-		anzahlGestellterFragen=value;
+		anzahlGestellterFragen = value;
 	}
 
 	@Override
@@ -59,7 +62,7 @@ public class TheoretischeInformatik extends Kurs {
 	public void setIstPruefungsFrage(boolean value) {
 		istPruefungsFrage = value;
 	}
-	
+
 	@Override
 	public String getName() {
 		return name;
@@ -74,11 +77,9 @@ public class TheoretischeInformatik extends Kurs {
 	public int getECTS() {
 		return 5;
 	}
-	
+
 	@Override
 	public String getBeschreibung() {
-		return "Das Ziel des Faches Theoretische Informatik ist es den Studierenden "
-				+ "eine Einfuehrung in Automatentheorie, formale Spracehn und "
-				+ "Berechenbarkeitstheorie zu geben.";
+		return beschreibung;
 	}
 }
