b3ce85d4d564f1df00dc9c77f73768af7a0bf881
jonasch98 <jonasch@hm.edu>
2019-01-05 16:39:49 +0100

    added FrageTest

---
+++ src/test/java/ScientificComputingTheGame/model/FrageTest.java
@@ -0,0 +1,23 @@
+package test.java.ScientificComputingTheGame.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import main.java.ScientificComputingTheGame.model.Frage;
+
+public class FrageTest {
+
+	private String[] testFrage = { "Frage", "moeglicheLsg", "Antwort" };
+	private Frage frage = new Frage(testFrage);
+
+	@Test
+	public void testAllMethods() {
+		Assert.assertSame(testFrage[0] + " Die Antwortmoeglichkeiten sind: " + testFrage[1], frage.getFrage());
+		Assert.assertSame(testFrage[2], frage.getAntwort());
+		// frageStellen?
+		// frageWiederholen?
+		// antwortAbwarten?
+		Assert.assertTrue(frage.antwortRichtig("Antwort"));
+	}
+
+}
