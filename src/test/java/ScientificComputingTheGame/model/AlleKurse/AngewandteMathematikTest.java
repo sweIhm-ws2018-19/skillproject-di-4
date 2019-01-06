d127910254d81e98a6da571df9a0bb62372bf73d
christovogt <c.vogt0@hm.edu>
2019-01-06 23:27:02 +0100

    adding tests

---
+++ src/test/java/ScientificComputingTheGame/model/AlleKurse/AngewandteMathematikTest.java
@@ -0,0 +1,48 @@
+package test.java.ScientificComputingTheGame.model.AlleKurse;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import main.java.ScientificComputingTheGame.model.Frage;
+import main.java.ScientificComputingTheGame.model.AlleKurse.AngewandteMathematik;
+
+public class AngewandteMathematikTest {
+
+	AngewandteMathematik angMatTest = new AngewandteMathematik();
+	
+	@Test
+	public void testAllMethods() {
+		angMatTest.setAnzahlGestellterFragen(2);
+		angMatTest.setAnzahlRichtigBeantworteterFragen(2);
+		angMatTest.setIstPruefungsFrage(true);
+		Assert.assertTrue(testGetFragen());
+		Assert.assertSame(2,angMatTest.getAnzahlGestellterFragen());
+		Assert.assertSame(2, angMatTest.getAnzahlRichtigBeantworteterFragen());
+		Assert.assertTrue(angMatTest.getIstPruefungsFrage());
+		Assert.assertSame("Analysis", angMatTest.getName());
+		Assert.assertSame(2, angMatTest.getAnzahlZuBeantwortenderFragen());
+		Assert.assertSame(5, angMatTest.getECTS());
+		Assert.assertSame(AngewandteMathematik.beschreibung, angMatTest.getBeschreibung());
+	}
+
+	
+	public boolean testGetFragen() {
+		int equals = 0;
+		ArrayList<Frage> fragen = angMatTest.getFragen();
+		for(int i=0; i < AngewandteMathematik.alleFragen.length;i++) {
+			for(int j=0; j < fragen.size();j++)
+			if((AngewandteMathematik.alleFragen[i][0]+" Die Antwortmoeglichkeiten sind: "+AngewandteMathematik.alleFragen[i][1]).equals(fragen.get(j).getFrage()) && 
+					AngewandteMathematik.alleFragen[i][2].equals(fragen.get(j).getAntwort())) {
+				equals++;
+			}
+		}
+		if(equals == fragen.size()) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+}
---
+++ src/test/java/ScientificComputingTheGame/model/AlleKurse/SoftwareEntwicklung1Test.java
@@ -0,0 +1,48 @@
+package test.java.ScientificComputingTheGame.model.AlleKurse;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import main.java.ScientificComputingTheGame.model.Frage;
+import main.java.ScientificComputingTheGame.model.AlleKurse.SoftwareEntwicklung1;
+
+public class SoftwareEntwicklung1Test {
+
+	SoftwareEntwicklung1 seTest = new SoftwareEntwicklung1();
+
+	@Test
+	public void testAllMethods() {
+		seTest.setAnzahlGestellterFragen(2);
+		seTest.setAnzahlRichtigBeantworteterFragen(2);
+		seTest.setIstPruefungsFrage(true);
+		Assert.assertTrue(testGetFragen());
+		Assert.assertSame(2, seTest.getAnzahlGestellterFragen());
+		Assert.assertSame(2, seTest.getAnzahlRichtigBeantworteterFragen());
+		Assert.assertTrue(seTest.getIstPruefungsFrage());
+		Assert.assertSame(SoftwareEntwicklung1.name, seTest.getName());
+		Assert.assertSame(2, seTest.getAnzahlZuBeantwortenderFragen());
+		Assert.assertSame(5, seTest.getECTS());
+		Assert.assertSame(SoftwareEntwicklung1.beschreibung, seTest.getBeschreibung());
+	}
+
+	public boolean testGetFragen() {
+		int equals = 0;
+		ArrayList<Frage> fragen = seTest.getFragen();
+		for (int i = 0; i < SoftwareEntwicklung1.alleFragen.length; i++) {
+			for (int j = 0; j < fragen.size(); j++)
+				if ((SoftwareEntwicklung1.alleFragen[i][0] + " Die Antwortmoeglichkeiten sind: "
+						+ SoftwareEntwicklung1.alleFragen[i][1]).equals(fragen.get(j).getFrage())
+						&& SoftwareEntwicklung1.alleFragen[i][2].equals(fragen.get(j).getAntwort())) {
+					equals++;
+				}
+		}
+		if (equals == fragen.size()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
