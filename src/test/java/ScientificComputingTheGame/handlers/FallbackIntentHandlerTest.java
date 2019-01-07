6049caa45c2f7654589c90ee3e1c0d6a515b7c62
christovogt <c.vogt0@hm.edu>
2019-01-07 18:12:39 +0100

    added tests again ;D

---
+++ src/test/java/ScientificComputingTheGame/handlers/FallbackIntentHandlerTest.java
@@ -0,0 +1,51 @@
+package test.java.ScientificComputingTheGame.handlers;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.response.ResponseBuilder;
+import com.amazon.ask.model.Response;
+
+import main.java.ScientificComputingTheGame.handlers.FallbackIntentHandler;
+
+public class FallbackIntentHandlerTest {
+	private FallbackIntentHandler handler;
+
+	@Before
+	public void setup() {
+		handler = new FallbackIntentHandler();
+	}
+
+	@Test
+	public void testCanHandle() {
+		final HandlerInput inputMock = Mockito.mock(HandlerInput.class);
+		when(inputMock.matches(any())).thenReturn(true);
+		Assert.assertTrue(handler.canHandle(inputMock));
+	}
+
+	@Test
+	public void testHandle() {
+		final HandlerInput inputMock = Mockito.mock(HandlerInput.class);
+		when(inputMock.getResponseBuilder()).thenReturn(new ResponseBuilder());
+
+		final Optional<Response> returnResponse = handler.handle(inputMock);
+
+		Assert.assertTrue(returnResponse.isPresent());
+
+		final Response response = returnResponse.get();
+		Assert.assertFalse(response.getShouldEndSession());
+		Assert.assertTrue(response.getOutputSpeech().toString()
+				.contains("Tut mir leid, das weiss ich nicht. Sage einfach Hilfe."));
+
+	}
+
+}
